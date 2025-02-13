c5b61d4ab81138053a1558a16d869d33015e2b98
Vitalina <vsinkovaa@gmail.com>
2025-02-13 10:47:17 +0200

    js

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -6,18 +6,17 @@
 // "10 входить в першу чверть"
 
 // const min = Math.floor(Math.random() * (59 - 0) + 0);
-// const number = Number(prompt('Enter number'));
-// const min = Math.floor(Math.random() * (59 - 0) + 0);
+// const number = Number(prompt("Enter number"));
 // if (min <= 15) {
-//     alert (`${min} входить в 1 чверть`);
-// } else if (min <=30) {
-//     alert (`${min} входить в 2 чверть`);
-// } else if (min <=45) {
-//     alert (`${min} входить в 3 чверть`);
+//   alert(`${min} входить в 1 чверть`);
+// } else if (min <= 30) {
+//   alert(`${min} входить в 2 чверть`);
+// } else if (min <= 45) {
+//   alert(`${min} входить в 3 чверть`);
 // } else {
-//     alert (`${min} входить в 4 чверть`);
+//   alert(`${min} входить в 4 чверть`);
 // }
-// console.log(num);
+// console.log(number);
 
 // !Завдання 3:
 // Змінна num може набувати 4 значення: '1', '2', '3' або '4'
@@ -30,23 +29,23 @@
 // "Вибачте, але ви маєте ввести значення від 1 до 4 включно".
 // Значення змінної result виведіть в консоль.
 
-// const num = prompt('Введіть значення від 1 до 4');
-// let result = '';
+// const num = prompt("Введіть значення від 1 до 4");
+// let result = "";
 // switch (num) {
-//     case '1':
-//         result = 'зима';
-//         break;
-//     case '2':
-//         result = 'весна';
-//         break;
-//     case '3':
-//         result = 'літо';
-//         break;
-//     case '4':
-//         result = 'осінь';
-//         break;
-//     default:
-//         result = 'Вибачте, але ви маєте ввести значення від 1 до 4 включно';
+//   case "1":
+//     result = "зима";
+//     break;
+//   case "2":
+//     result = "весна";
+//     break;
+//   case "3":
+//     result = "літо";
+//     break;
+//   case "4":
+//     result = "осінь";
+//     break;
+//   default:
+//     result = "Вибачте, але ви маєте ввести значення від 1 до 4 включно";
 // }
 // console.log(result);
 
@@ -124,51 +123,23 @@
 //   alert("Я вас не знаю");
 // }
 
-// Завдання 1:
-// Напишіть функцію isAdult(age), яка  приймає число - вік користувача і повертає true,
-// якщо параметр age більше чи дорівнює 18.
-// В іншому випадку вона запитує підтвердження через confirm
-// і повертає його результат (true/false).
-// 1
-// function isAdult (age) {
-//     if (age >= 18) {
-//        return true;
-//     } else {
-//         const checkAge = confirm('Are you adult?');
-//         return checkAge;
-//     }
-// }
-// 2
-// function isAdult (age) {
-//     if (age >= 18) {
-//        return true;
-//     }
-//     return confirm('Are you adult?');
-// }
-// 3
-// function isAdult (age) {
-//      return age >= 18 ? true: confirm('Are you adult?');
-
-//      4
-//     return age >= 18 || confirm('Are you adult?');
-// }
-
-// console.log(isAdult(25));
+//
 
 // Напишіть функцію min(a, b), яка приймає 2 числа і повертає меньше з них.
 // Потрібно додати перевірку, що функція отримує саме числа, в іншому випадку
 // повертати з функції рядок - 'Not a number!'.
 
-// function min(a, b) {
-//     if (typeof a !== 'number' || typeof b !== 'number') {
-//         return 'Not a number!';
-//     }
-//     return Math.min(a, b);
-// }
-// console.log(min(5, 10));
+function min(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    return "Not a number!";
+  }
+  return Math.min(a, b);
+}
+console.log(min(15, 10));
 
 // function min(a, b) {
-//     return (typeof a !== 'number' || typeof b !== 'number') ? 'Not a number!': Math.min(a, b);
+//   return typeof a !== "number" || typeof b !== "number"  ? "Not a number!"
+//     : Math.min(a, b);
 // }
 
 // Напишіть функцію getNumbers(min, max), що приймає 2 параметри -
@@ -202,16 +173,56 @@
 // Потрібно додати перевірку, що функція отримує саме числа, в іншому випадку
 // повертати з функції рядок - 'Not a number!'.
 
-function min(a, b) {
-  if (typeof a === "number" && typeof b === "number") {
-    if (a < b) {
-      return a;
-    } else if (a > b) {
-      return b;
-    } else {
-      return "Not a number!";
-    }
-  }
-}
+// function min(a, b) {
+//   if (typeof a === "number" && typeof b === "number") {
+//     if (a < b) {
+//       return a;
+//     } else if (a > b) {
+//       return b;
+//     } else {
+//       return "Not a number!";
+//     }
+//   }
+// }
 
-console.log(min(5.5, 10));
+// console.log(min(15, 13));
+
+// !Завдання 9:
+
+// Напишіть функцію isAdult(age), яка  приймає число - вік користувача і повертає true,
+// якщо параметр age більше чи дорівнює 18.
+// В іншому випадку вона запитує підтвердження через confirm
+// і повертає його результат (true/false).
+
+// *1
+// function isAdult (age) {
+//     if (age >= 18) {
+//        return true;
+//     } else {
+//         const checkAge = confirm('Are you adult?');
+//         return checkAge;
+//     }
+// }
+// *2
+// function isAdult (age) {
+//     if (age >= 18) {
+//        return true;
+//     }
+//     return confirm('Are you adult?');
+// }
+// *3
+// function isAdult(age) {
+//   return age >= 18 ? true : confirm("Are you adult?");
+
+//  *4;
+//   return age >= 18 || confirm("Are you adult?");
+// }
+
+// console.log(isAdult(15));
+
+// !Завдання 10:
+
+// Напишіть функцію fizzBuzz(num), яка приймає число і перевіряє кожне число від 1 до num:
+// Якщо число ділитися  без остачі на 3 - виводить в консоль 'fizz',
+// якщо ділиться  без остачі на 5 - виводить в консоль 'buzz',
+// якщо ділиться  без остачі і на 3, і на 5 - виводить в консоль 'fizzbuzz'.
